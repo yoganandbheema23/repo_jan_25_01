5bb2bd4874c50334569b552c9ebed1272bb1f8ee
testingpoint09 <testingpoint09@gmail.com>
2024-01-25 12:28:20 +0530

    new project is added by testing point team

---
+++ src/main/java/com/project25/App.java
@@ -0,0 +1,13 @@
+package com.project25;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
